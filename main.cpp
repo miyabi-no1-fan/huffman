dd189b8c98b7e1a9116d0450e012ccbd8c46f5d2
miyabi-no1-fan <222554062+miyabi-no1-fan@users.noreply.github.com>
2026-08-25 01:03:11 +0700

    add a for loop testing if Huffman lookup table works

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
 
 #include "huffman.hpp"
@@ -7,5 +8,19 @@ int main() {
     const char msg[] = "ABBCCCDDDDEEEEEFFFFFFGGGGGGGHHHHHHHH";
     auto dict = Huffman::make_dictionary((uint8_t*)msg, sizeof(msg) - 1);
     std::cout << dict;
+    auto table = Huffman::make_lookup_table(dict);
+    
+    for (int i = 0; i < dict.size(); i++) {
+        if (dict[i].length > 0) {
+            auto code = dict[i].code;
+            auto len = dict[i].length;
+            auto symbol = dict[i].symbol;
+
+            if (symbol != (*table)[code << (16 - len)].symbol ||
+                len != (*table)[code << (16 - len)].length)
+                std::abort();
+        }
+    }
+
     return 0;
 }
